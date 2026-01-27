05f40c94edf8b2b4c182d1ccedc5d8ad7913cc64
J LIN <116197057+jayelinjiaqi@users.noreply.github.com>
2026-01-27 10:18:29 +0800

    Update jayelin-EV.sql

--- jayelin-EV.sql
+++ jayelin-EV.sql
@@ -1,3 +1,24 @@
+
+-- ===================================================================== EV Total ===================================================================
+-- $1,626,326
+select sum(pt.tran_amount/100)
+from parking_transaction pt
+JOIN evcharging evc on pt.transaction_id = evc.id
+JOIN car_park cp ON pt.car_park_id = cp.id
+JOIN cp_indent ci on evc.order_id = ci.session_id
+WHERE convert_tz(pt.exit_date_time, '+00:00', '+08:00') >= '2025-01-01 00:00:00' and convert_tz(pt.exit_date_time, '+00:00', '+08:00') <= '2025-12-31 23:59:59'
+and pt.payment_status = 1
+-- ===================================================================== EV Tran Amt by Month ===================================================================
+select date_format(convert_tz(pt.exit_date_time, '+00:00', '+08:00'), '%Y-%m'), sum(tran_amount/100), sum(ci.power), count(*)
+from parking_transaction pt
+JOIN evcharging evc on pt.transaction_id = evc.id
+JOIN car_park cp ON pt.car_park_id = cp.id
+JOIN cp_indent ci on evc.order_id = ci.session_id
+WHERE convert_tz(exit_date_time, '+00:00', '+08:00') >= '2025-01-01 00:00:00' and convert_tz(exit_date_time, '+00:00', '+08:00') <= '2025-12-31 23:59:59'
+and pt.payment_status = 1
+GROUP BY date_format(convert_tz(pt.exit_date_time, '+00:00', '+08:00'), '%Y-%m')
+order by date_format(convert_tz(pt.exit_date_time, '+00:00', '+08:00'), '%Y-%m')
+
 -- ===================================================================== All Chargers PPU Users ===================================================================
 
 select date_format(convert_tz(exit_date_time, '+00:00', '+08:00'), '%Y-%m'), sum(tran_amount/100), sum(ci.power), count(*)
@@ -76,11 +97,40 @@ from parking_transaction pt
 JOIN evcharging evc on pt.transaction_id = evc.id
 JOIN car_park cp ON pt.car_park_id = cp.id
 JOIN cp_indent ci on evc.order_id = ci.session_id
-WHERE pt.car_park_id in (select id from car_park where car_park_operator_id = 3)
-and convert_tz(exit_date_time, '+00:00', '+08:00') >= '2025-01-01 00:00:00' and convert_tz(exit_date_time, '+00:00', '+08:00') <= '2025-12-31 23:59:59'
+-- WHERE pt.car_park_id in (select id from car_park where car_park_operator_id = 3)
+WHERE convert_tz(exit_date_time, '+00:00', '+08:00') >= '2025-01-01 00:00:00' and convert_tz(exit_date_time, '+00:00', '+08:00') <= '2025-12-31 23:59:59'
 and pt.payment_status = 1
 group by region
 order by sum(ci.power) desc
+
+-- ===================================================================== Count Chargers by Region ===================================================================================
+select CASE
+WHEN cp.name IN ('(Private) Currency House','(Private) EVOne [DSO]','(Private) EVOne [Sulisam Investments]','(Private) Holt Residences','(Private) MAS Building',
+	'(Private) N','(Private) Novena Regency','(Private) One Leicester','(Private) One Robin','(Private) Parc Stevens','(Private) ST Elec Hub','(Private) State Courts of Singapore',
+	'(Private) Sunglade Condo','(Private) Tan Tock Meng Tower','AIA Alexandra','AIA Tower','LTA - Corporate HQ','NTU EV [LKC Medicine]','NTU EV [One North]','TOYOGO HQ') THEN 'Central'
+WHEN cp.name IN ('(Private) AIA Tampines', '(Private) BluWaters 2 Condo', '(Private) Eastwood Regency', '(Private) EVOne [KLN Logistics Tampines]', 'EVOne [SIA Airline House]',
+    'EVOne [SIA Training Center]', 'EVOne [StorHub Changi]') THEN 'East'
+WHEN cp.name IN ('(Private) CCL Design', '(Private) EVOne [ TYJ ]', 'EVOne [ StorHub Marsiling ]', 'EVOne [Harvest Woodlands]') THEN 'North'
+WHEN cp.name IN ('(Private) EVOne [DHL Greenwich]','(Private) Kovan Residences','(Private) ST Engineering Hub AMK','(Private) The Waterline','EVOne [ Jalan Pemimpin ]','LTA - Sin Ming Office',
+	'Natural Cool Defu','Union Gas [ Defu Lane ]') THEN 'North-East'
+WHEN cp.name IN ('(Private) Katoen Natie (Jurong)','(Private) Regent Grove','(Private) ST Land Systems JBL','(Private) STEngg JE Hub','(Private) STEngg Marine Benoi','(Private) STEngg Marine Gul Rd',
+	'(Private) Varsity Park Condo','EVOne [ Aste Global ]','EVOne [ DArena ]','EVOne [ Pioneer Center ]','EVOne [ StorHub Jurong East ]','EVOne [ Toh Guan #111 ]','EVOne [ Toh Guan #150 ]',
+	'EVOne [Cnergy]','GoParkin Testing',
+	'NTU EV [ Car Park A ]','NTU EV [ Car Park B ]','NTU EV [ Car Park Crescent ]','NTU EV [ Car Park D ]','NTU EV [ Car Park North Hill ]','NTU EV [ Car Park Q ]','NTU EV [ Car Park SBS ]',
+	'YCH Bulim') THEN 'West'
+ELSE concat(cp.name,' latitude',cp.latitude,' longitude',cp.longitude)
+END AS region,
+count(distinct(ci.stake_id))
+from parking_transaction pt
+JOIN evcharging evc on pt.transaction_id = evc.id
+JOIN car_park cp ON pt.car_park_id = cp.id
+JOIN cp_indent ci on evc.order_id = ci.session_id
+and convert_tz(exit_date_time, '+00:00', '+08:00') >= '2025-01-01 00:00:00' and convert_tz(exit_date_time, '+00:00', '+08:00') <= '2025-12-31 23:59:59'
+and pt.payment_status = 1
+group by region
+order by count(distinct(ci.stake_id)) desc
+
+select * from cp_indent limit 1
 
 -- ========================================================== EV Tran Amt, Power, Vol, Avg Tran Price, Charge Price by Commercial / Private ==========================================================
 select CASE
@@ -104,7 +154,7 @@ from parking_transaction pt
 JOIN evcharging evc on pt.transaction_id = evc.id
 JOIN car_park cp ON pt.car_park_id = cp.id
 JOIN cp_indent ci on evc.order_id = ci.session_id
-WHERE pt.car_park_id in (select id from car_park where car_park_operator_id = 3)
+-- WHERE pt.car_park_id in (select id from car_park where car_park_operator_id = 3)
 and convert_tz(exit_date_time, '+00:00', '+08:00') >= '2025-01-01 00:00:00' and convert_tz(exit_date_time, '+00:00', '+08:00') <= '2025-12-31 23:59:59'
 and pt.payment_status = 1
 group by CondoComPrivate
